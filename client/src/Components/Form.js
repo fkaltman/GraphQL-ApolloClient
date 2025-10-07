83131c5843faf541f1097c8c881eb2df86d1e10b
Francine Altman <francinekline@gmail.com>
2025-10-07 12:16:12 -0400

    fix: form added value props to form components to fix form reset

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -65,6 +65,7 @@ function Form() {
 
       <input
         type="text"
+        value={firstName}
         placeholder="First Name"
         onChange={(e) => {
           setFirstName(e.target.value);
@@ -72,6 +73,7 @@ function Form() {
       />
       <input
         type="text"
+        value={lastName}
         placeholder="Last Name"
         onChange={(e) => {
           setLastName(e.target.value);
@@ -79,6 +81,7 @@ function Form() {
       />
       <input
         type="text"
+        value={email}
         placeholder="Email"
         onChange={(e) => {
           setEmail(e.target.value);
@@ -86,6 +89,7 @@ function Form() {
       />
       <input
         type="text"
+        value={password}
         placeholder="Password"
         onChange={(e) => {
           setPassword(e.target.value);
